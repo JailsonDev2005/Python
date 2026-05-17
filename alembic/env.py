98654e12aa6e850405523b20921ef115cece42b9
Jailson santos da silva <devjailson43@gmail.com>
2026-05-17 15:09:02 -0300

    projeto crud2 atualizado

--- alembic/env.py
+++ alembic/env.py
@@ -8,7 +8,7 @@ from alembic import context
 import sys
 import os
 
-sys.path.append(os.path.abspath())
+sys.path.append(os.path.abspath(os.getcwd()))
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -23,7 +23,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from models import base
+target_metadata = base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
