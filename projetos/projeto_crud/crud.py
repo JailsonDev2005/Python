e68ea025d5fd328d746ac7b55b03921f980d34ed
Jailson santos da silva <devjailson43@gmail.com>
2026-05-17 19:56:56 -0300

    Finalizado

---
+++ projetos/projeto_crud/crud.py
@@ -0,0 +1,53 @@
+from defs import *
+
+while True:
+    menu()
+
+    try:
+
+        escolha = int(input("Digite uma Opção: "))
+
+    except ValueError:
+
+        print("[red]Digite apenas números![/]")
+        continue
+
+    if escolha == 1:
+
+        nome = str(input("Digite seu Nome: ")).strip()
+        idade = ler_inteiro("Digite sua idade: ")
+                
+        sexo = ''
+        while sexo not in ["M", "F"]:
+            sexo = str(input("Digite seu sexo[F|M]: ")).strip().upper()
+        numero = ler_inteiro("Digite seu número: ")
+        criar_cadastro(nome, idade, sexo, numero)
+
+    elif escolha == 2:
+        
+        lista_cadastro()
+
+    elif escolha == 3:
+
+        indice = ler_indice("Digite o indice para alterar: ")
+        nome_novo = str(input("Novo nome:")).strip()
+        idade_nova = ler_inteiro("Idade novo: ")
+        sexo_novo = ''
+        while sexo_novo not in ["M", "F"]:
+            sexo_novo = input("Sexo novo: ").strip().upper()
+        numero_novo = ler_inteiro("novo número: ")
+        atualizar_cadastro(indice, nome_novo, idade_nova, sexo_novo, numero_novo)
+
+    elif escolha == 4:
+
+        indice = ler_indice("Qual indice deseja excluir: ")
+        excluir_cadastro(indice)
+
+    elif escolha == 5:
+
+        print("Sistema Cadastro encerrado")
+        break
+    
+    else:
+        print("[red]Digite um Opção válida[/]")
+    
--- projetos/projeto_crud/defs.py
+++ projetos/projeto_crud/defs.py
@@ -2,18 +2,34 @@ from rich import print
 from rich.table import Table
 from rich.panel import Panel
 
+#GUARDA OS DADOS DO USUARIO
 Dados = []
 
+#MOSTRA O MENU DE OPÇÕES NO TERMINAL
+def menu():
 
+    conteudo = "Opções"
+    conteudo += "\n1. Criar novo cadastro"
+    conteudo += "\n2. Lista cadastro"
+    conteudo += "\n3. Atualizar cadastro"
+    conteudo += "\n4. Excluir cadastro"
+    conteudo += "\n5. Sair"
 
+    menu = Panel(conteudo,title="Sistema CRUD", width=28)
+
+    print(menu)
+
+#CRIA O CADASTRO DO USUARIO
 def criar_cadastro(nome, idade, sexo, numero):
+
     cadastro = {"nome":nome, "idade":idade, "sexo":sexo, "numero":numero}
     Dados.append(cadastro)
 
     print("[bold green]Cadastro realizado com sucesso![/]")
 
-
+#LISTA O USUARIO
 def lista_cadastro():
+
     if not Dados:
         print("[red]Nenhum cadastro encontrado[/]")
         return
@@ -38,7 +54,9 @@ def lista_cadastro():
     print(tabela)
 
 
+#EDITA O CADASTRO DO USUARIO
 def atualizar_cadastro(i, nome, idade, sexo, numero):
+
     if 0 <= i < len(Dados):
         Dados[i].update({
             "nome": nome,
@@ -50,10 +68,12 @@ def atualizar_cadastro(i, nome, idade, sexo, numero):
         print("[green]Cadastro atualizado com sucesso![/]")
 
     else:
+
         print("[red]Atualização inválida[/]")
 
-
+#EXCLUI O CADASTRO DO USUARIO
 def excluir_cadastro(i):
+
     if 0 <= i < len(Dados):
         cadastro_excluido = Dados.pop(i)
         print(f"[green]Cadastro {cadastro_excluido['nome']} removido com sucesso![/]")
@@ -61,15 +81,18 @@ def excluir_cadastro(i):
         print("[red]Erro ao remover[/]")
 
 
+#LER SE O NÚMERO E INTEIOR
 def ler_inteiro(msg):
+
     while True:
         try:
             return int(input(msg))
         except ValueError:
             print("[red]Digite apenas números[/]")
 
-
+#LER O INDICE DIGITADO PELO USUARIO
 def ler_indice(msg):
+
     while True:
         try:
             i = int(input(msg)) - 1
@@ -79,60 +102,3 @@ def ler_indice(msg):
 
         except ValueError:
             print("[red]Digite apenas números[/]")
-
-
-
-while True:
-    conteudo = "Opções"
-    conteudo += "\n1. Criar novo cadastro"
-    conteudo += "\n2. Lista cadastro"
-    conteudo += "\n3. Atualizar cadastro"
-    conteudo += "\n4. Excluir cadastro"
-    conteudo += "\n5. Sair"
-    menu = Panel(conteudo,title="Sistema CRUD", width=28)
-
-    print(menu)
-
-    try:
-        escolha = int(input("Digite uma Opção: "))
-    except ValueError:
-        print("[red]Digite apenas números![/]")
-        continue
-
-    if escolha == 1:
-        nome = str(input("Digite seu Nome: ")).strip()
-        idade = ler_inteiro("Digite sua idade: ")
-                
-        sexo = ''
-        while sexo not in ["M", "F"]:
-            sexo = str(input("Digite seu sexo[F|M]: ")).strip().upper()
-        numero = ler_inteiro("Digite seu número: ")
-        criar_cadastro(nome, idade, sexo, numero)
-
-    elif escolha == 2:
-        lista_cadastro()
-
-    elif escolha == 3:
-
-        indice = ler_indice("Digite o indice para alterar: ")
-        nome_novo = str(input("Novo nome:")).strip()
-        idade_nova = ler_inteiro("Idade novo: ")
-        sexo_novo = ''
-        while sexo_novo not in ["M", "F"]:
-            sexo_novo = input("Sexo novo: ").strip().upper()
-        numero_novo = ler_inteiro("novo número: ")
-        atualizar_cadastro(indice, nome_novo, idade_nova, sexo_novo, numero_novo)
-
-    elif escolha == 4:
-
-        indice = ler_indice("Qual indice deseja excluir: ")
-        excluir_cadastro(indice)
-
-    elif escolha == 5:
-
-        print("Sistema Cadastro encerrado")
-        break
-    
-    else:
-        print("[red]Digite um Opção válida[/]")
-    
