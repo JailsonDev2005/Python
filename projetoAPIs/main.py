326ac3793df17df6f5e91b6c45f3093517153cfb
Jailson santos da silva <devjailson43@gmail.com>
2026-05-17 23:42:48 -0300

    finalizado

--- projetoAPIs/main.py
+++ projetoAPIs/main.py
@@ -8,6 +8,8 @@ from typing import List, Optional
 #CRIAR API
 app = FastAPI(title="API de Tarefas")
 
+#uvicorn projetoAPIs.main:app --reload
+
 #MODELO DE DADOS
 class Tarefa(BaseModel):
     id: Optional[int] = None
@@ -30,4 +32,33 @@ async def criar_tarefa(tarefa: Tarefa):
     db_tarefas.append(tarefa)
     return tarefa
 
+#BUSCAR TAREFA POR ID
+@app.get('tarefa/{tarefa_id}', response_model=Tarefa)
+async def obter_tarefa(tarefa_id: int):
+    for t in db_tarefas:
+        if t.id == tarefa_id:
+            return t
+    raise HTTPException(status_code=404, detail="Tarefa não encontrada")
 
+#ROTA PARA ALTERA
+@app.put('/tarefas/{tarefas_id}', response_model=Tarefa)
+async def atualizar_tarefa(tarefa_id: int, tarefa_atualizar: Tarefa):
+    for index, t in enumerate(db_tarefas):
+        if t.id ==tarefa_id:
+            tarefa_atualizar.id = tarefa_id
+
+            db_tarefas[index] = tarefa_atualizar
+
+            return tarefa_atualizar
+    
+    raise HTTPException(status_code=404, detail="Tarefa não encontrado")
+
+
+#ROTA PARA DELETAR
+@app.delete('/tarefas/{tarefas_id}', status_code=204)
+async def delete_tarefa(tarefa_id: int):
+    for t in db_tarefas:
+        if t.id == tarefa_id:
+            db_tarefas.remove(t)
+            return
+    raise HTTPException(status_code=404)
