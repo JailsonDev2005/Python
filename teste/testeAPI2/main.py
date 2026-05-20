9ab11ae255e5336c2d95fcf0a2c7b59189434a58
Jailson santos da silva <devjailson43@gmail.com>
2026-05-20 16:24:27 -0300

    projeto api2 concluido

--- teste/testeAPI2/main.py
+++ teste/testeAPI2/main.py
@@ -29,3 +29,41 @@ async def criar_tarefa(tarefa: Tarefa):
     tarefa.id = len(db_tarefas) + 1
     db_tarefas.append(tarefa)
     return tarefa
+
+#BUSCAR TAREFA POR ID
+@app.get('/tarefas/{tarefas_id}', response_model=Tarefa)
+async def obter_tarefa(tarefa_id: int):
+    for t in db_tarefas:
+        if t.id == tarefa_id:
+            return t 
+    raise HTTPException(status_code=404, detail="Tarefa não encontrada")
+
+#ROTA PARA ALTERAR TAREFA
+@app.put('/tarefas/{tarefa_id}', response_model=Tarefa)
+async def atualizar_tarefa(
+    tarefa_id: int,
+    tarefa_atualizada: Tarefa
+):
+    for index, t in enumerate(db_tarefas):
+
+        if t.id == tarefa_id:
+
+            tarefa_atualizada.id = tarefa_id
+
+            db_tarefas[index] = tarefa_atualizada
+
+            return tarefa_atualizada
+
+    raise HTTPException(
+        status_code=404,
+        detail="Tarefa não encontrada"
+    )
+
+#ROTA PARA DELETAR
+@app.delete('/tarefas/{tarefa_id}', status_code=204)
+async def deleta_tarefa(tarefa_id: int):
+    for t in db_tarefas:
+        if t.id == tarefa_id:
+            db_tarefas.remove(t)
+            return
+    raise HTTPException(status_code=404, detail="Tarefa não encontrada")
